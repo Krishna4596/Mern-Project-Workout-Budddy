7d3f4234f00035da194ad7a034c01d6313ee0975
krishna4596 <krishprajapat99977@gmail.com>
2026-02-09 12:54:29 +0530

    Fixing frontend folder display issue

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,63 @@
+// import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
+
+//component
+import WorkoutDetails from '../components/WorkoutDetails'
+import WorkoutForm from '../components/WorkoutForm'
+import { useWorkoutsContext } from '../hooks/useWorkoutsContext'
+import {useAuthContext} from '../hooks/useAuthContext'
+
+
+
+const Home = () => {
+
+  //useState 
+  // const [workouts, setWorkouts] = useState(null)
+
+  const {workouts, dispatch} = useWorkoutsContext();
+  const {user} = useAuthContext()
+
+  useEffect(() => {
+    const fetchWorkouts = async () => {
+     const response =  await fetch('/api/workouts/', {
+      headers: {
+        'Authorization': `Bearer ${user.token}`
+      }
+     })
+     const json = await response.json()
+
+     if(response.ok){
+      //useState
+      // setWorkouts(json)
+
+      dispatch({type: 'SET_WORKOUTS', payload: json})
+
+     }
+    }
+
+    if(user){
+      fetchWorkouts()
+    }
+    
+  }, [dispatch, user])
+
+
+  return (
+    <div className='home'>
+        <div className='workouts'>
+          {
+            workouts && workouts.map((workout)=>(
+              // <p key={workout._id}>{workout.title}</p>
+              <WorkoutDetails key={workout._id} workout={workout} />
+              
+            ))
+          }
+        </div>
+        <div>
+          <WorkoutForm/>
+        </div>
+    </div>
+  )
+}
+
+export default Home
